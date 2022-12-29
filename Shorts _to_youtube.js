d2e65b05c149a34b3b5682d1ee67d709b8ebae8e
Ezinko <85968010+Ezinko@users.noreply.github.com>
2022-12-29 14:56:10 -0300

    Create Shorts _to_youtube.js
    The code

---
+++ Shorts _to_youtube.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        Shorts To Youtube video
+// @namespace   Violentmonkey Scripts
+// @match       https://www.youtube.com/*
+// @version     1.0
+// @author      Kouta e Bozo
+// @description 29/12/2022 10:25:36
+// ==/UserScript==
+
+function replaceURL() {
+  var theURL = window.location.pathname;
+  if(theURL.includes('shorts')){
+    var newURL = theURL.replace('shorts/', 'watch?v=');
+    window.location = "https://www.youtube.com" + newURL;
+  }
+};
+
+document.addEventListener('yt-navigate-start', function() {
+  console.log('cliquei');
+  replaceURL();
+}, false);
