69c50ab70db78b651576db281f8042a916005c49
Ezinko <85968010+Ezinko@users.noreply.github.com>
2022-12-31 11:36:36 -0300

    Update Shorts_to_youtube.js
    Did a change where you could get in a infinite loop if you tried to open the shorts tab in a channel, so now it's possible to load the shorts page without the infinite code load to change the URL. The only place I believe(hope) it'll change is in the shorts itself.

--- Shorts_to_youtube.js
+++ Shorts_to_youtube.js
@@ -2,14 +2,14 @@
 // @name        Shorts To Youtube video
 // @namespace   Violentmonkey Scripts
 // @match       https://www.youtube.com/*
-// @version     1.0
+// @version     1.2
 // @author      Kouta e Bozo
 // @description 29/12/2022 10:25:36
 // ==/UserScript==
 
 function replaceURL() {
   var theURL = window.location.pathname;
-  if(theURL.includes('shorts')){
+  if(theURL.includes('shorts/')){
     var newURL = theURL.replace('shorts/', 'watch?v=');
     window.location = "https://www.youtube.com" + newURL;
   }
